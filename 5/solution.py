ac87e3d91504273e36ffa146d554130b1348f02c
Víctor López Ferrando <victor@iomob.net>
2020-12-05 08:47:42 +0100

    Solved problem 5

---
+++ 5/solution.py
@@ -0,0 +1,9 @@
+# Part 1
+ids = [int(l.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2)
+       for l in open('input').read().splitlines()]
+print(max(ids))
+
+# Part 2
+for i in ids:
+    if i+1 not in ids and i+2 in ids:
+        print(i+1)
