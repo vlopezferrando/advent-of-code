fc0b819c47de7da7bddc5343170938950c7c51b4
Víctor López Ferrando <victor@iomob.net>
2020-12-06 08:28:55 +0100

    Add solution to problem 6

---
+++ 6/solution.py
@@ -0,0 +1,13 @@
+# Read input
+groups = [[]]
+for line in open('input').read().splitlines():
+    if line == '':
+        groups.append([])
+    else:
+        groups[-1].append(line)
+
+# Part 1
+print(sum(len(set(''.join(g))) for g in groups))
+
+# Part 2
+print(sum(sum(all(letter in g for g in group) for letter in group[0]) for group in groups))
