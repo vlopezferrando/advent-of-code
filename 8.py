bb3af18601d5c2ea6a8391eb85d451d94a7e591c
Víctor López Ferrando <victor@iomob.net>
2020-12-08 23:19:46 +0100

    Solve problem 8

---
+++ 8.py
@@ -0,0 +1,23 @@
+# Read input
+rows = [[l.split()[0], int(l.split()[1])] for l in open('8.input')]
+
+# Part 1
+def run():
+    seen = set()
+    i = acc = 0
+    while i < len(rows) and i not in seen:
+        seen.add(i)
+        acc += rows[i][1] if rows[i][0] == 'acc' else 0
+        i += rows[i][1] if rows[i][0] == 'jmp' else 1
+    return acc, i
+
+print(run()[0])
+
+# Part 2
+FLIP = {'jmp': 'nop', 'nop': 'jmp', 'acc': 'acc'}
+for row in rows:
+    row[0] = FLIP[row[0]]
+    acc, last_i = run()
+    if last_i == len(rows):
+        print(acc)
+    row[0] = FLIP[row[0]]
