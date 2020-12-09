bbf0f02ad6c0ad53d24c9a990cc3bf213423c476
Víctor López Ferrando <victor@iomob.net>
2020-12-09 08:08:04 +0100

    Solve problem 9

---
+++ 9.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+# Read input
+v = [int(l) for l in open('9.in')]
+
+# Part 1
+for i, x in enumerate(v[25:]):
+    if all(sum(c) != x for c in combinations(v[i:i+25], 2)):
+        invalid = x
+        break
+print(invalid)
+
+# Part 2
+for i in range(len(v)-2):
+    for j in range(i+2, len(v)):
+        if sum(v[i:j]) == invalid:
+            print(min(v[i:j]) + max(v[i:j]))
