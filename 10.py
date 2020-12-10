ca864e680b04944e22769dac7d3627de7d7f3d94
Víctor López Ferrando <victor@iomob.net>
2020-12-10 11:09:17 +0100

    Solved problem 10

---
+++ 10.py
@@ -0,0 +1,13 @@
+# Read input
+v = sorted([int(l) for l in open('10.in')])
+v = [0] + v + [v[-1]+3]
+
+# Part 1
+print(sum(b-a == 1 for a, b, in zip(v, v[1:])) * sum(b-a == 3 for a, b, in zip(v, v[1:])))
+
+# Part 2
+ways = [1] + [0]*v[-1]
+for n in v:
+    for m in range(max(n-3, 0), n):
+        ways[n] += ways[m]
+print(ways[-1])
