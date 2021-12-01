2a75acade3a65ddb6ff6097f1ddb37c877229a94
Víctor López Ferrando <victor@iomob.net>
2021-12-01 15:54:28 +0100

    Move 2021 solutions to subdirectory

--- 2021/advent2021.py
+++ 2021/advent2021.py
@@ -19,7 +19,7 @@ import re
 
 def data(day: int, parser=str, sep='\n') -> list:
     "Split the day's input file into sections separated by `sep`, and apply `parser` to each."
-    sections = open(f'input/2021/{day}.txt').read().rstrip().split(sep)
+    sections = open(f'input/{day}.txt').read().rstrip().split(sep)
     return [parser(section) for section in sections]
 
 
